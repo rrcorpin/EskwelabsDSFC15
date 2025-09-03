9405f671ec4afdda8ee9b381f0f7f3e979a1e7d2
RR <rrcorpin18@gmail.com>
2025-09-03 19:56:31 +0800

    Revised code

--- Sprint1_Presentation.py
+++ Sprint1_Presentation.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import os
+# import matplotlib.pyplot as plt
+# import os
 
 # Set page title
 st.set_page_config(page_title="My Streamlit App", layout="wide")
